83ca6525433e126b6757da65858012996fb2c282
Fahad Khan <fahadkhan1740@outlook.com>
2016-06-27 14:43:49 +0530

    Tickets processing complete

---
+++ app/Ticket.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    protected $fillable = ['title','content','slug','status','user_id'];
+    
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+{!! Form::open(array ('url' => 'contact')) !!}
+{!! Form::label('First name') !!}
+{!! Form::text('firstname', 'Enter your first name') !!}
+<br />
+{!! Form::label('Last name') !!}
+{!! Form::text('lastname', 'Enter your last name') !!}
+<br />
+{!! Form::submit() !!}
+{!! Form::close() !!}
